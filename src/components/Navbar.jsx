8517cdf298361b00db6e52e19884296b6e4fbae0
Guillaume Sadourny <guillaume.sadourny@ecoles-epsi.net>
2025-02-15 11:14:59 +0100

    feat: add About section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,13 +21,16 @@ const Navbar = () => {
           <a href="#Header" className="cursor-pointer hover:text-gray-400">
             Home
           </a>
-          <a href="#Header" className="cursor-pointer hover:text-gray-400">
+          <a href="#About" className="cursor-pointer hover:text-gray-400">
             About
           </a>
-          <a href="#Header" className="cursor-pointer hover:text-gray-400">
+          <a href="#Projects" className="cursor-pointer hover:text-gray-400">
             Projects
           </a>
-          <a href="#Header" className="cursor-pointer hover:text-gray-400">
+          <a
+            href="#Testimonials"
+            className="cursor-pointer hover:text-gray-400"
+          >
             Testimonials
           </a>
         </ul>
